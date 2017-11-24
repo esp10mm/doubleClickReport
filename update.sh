1a3b81c00eddcf5f1f6be9ba06eeaaa416d84368
Patrick Kuo <patrick5267@gmail.com>
2017-11-24 12:02:30 +0800

    update

--- update.sh
+++ update.sh
@@ -2,3 +2,4 @@ curl https://localhost:8051/videoreport -k -o video_report.csv
 curl https://localhost:8051/displayreport -k -o display_report.csv
 git add -A
 git commit -m 'update'
+git push origin gh-pages
