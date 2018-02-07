df3288b9ad45b3faf9a4d7223a6f9badaefaefc4
Patrick Kuo <patrick5267@gmail.com>
2018-02-07 10:55:48 +0800

    Revert "update"
    This reverts commit 978717235d1277aa39d5ecb3b80b1d176d1a21f5.

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-curl https://localhost:8051/videoreport -k -o video_report.csv
-curl https://localhost:8051/displayreport -k -o display_report.csv
-git add -A
-git commit -m 'update'
+# curl https://localhost:8051/videoreport -k -o video_report.csv
+# curl https://localhost:8051/displayreport -k -o display_report.csv
+# git add -A
+# git commit -m 'update'
 git push origin gh-pages
